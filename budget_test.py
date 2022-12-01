fbf8d612ce3eb1c79a89116f71381098ee89e3af
lucaslsg-prog <lsg.comunicacoes@gmail.com>
2022-11-30 22:05:35 -0300

    Add invalid name test function

--- budget_test.py
+++ budget_test.py
@@ -22,6 +22,9 @@ class AndroidBudget(unittest.TestCase):
 
     def test_app_budget_add(self):
 
+        test_names= [""," ",12,"Lucas"]
+        test_values= [" ","","teste_letras",1000]
+
         if self.driver.find_element(By.XPATH, "//android.widget.TextView[contains(@text, 'Welcome to Budget Watch')]"):
             skip = self.driver.find_element(By.ID, 'protect.budgetwatch:id/skip')
             skip.click()
@@ -43,10 +46,10 @@ class AndroidBudget(unittest.TestCase):
             add.click()
             
             name = self.driver.find_element(By.ID, 'protect.budgetwatch:id/budgetNameEdit')
-            name.set_text("teste")
+            name.set_text(test_names[3])
 
             value = self.driver.find_element(By.ID, 'protect.budgetwatch:id/valueEdit')
-            value.set_text("1000")
+            value.set_text(test_values[3])
 
             save = self.driver.find_element(By.ID, 'protect.budgetwatch:id/action_save')
             save.click()
@@ -56,8 +59,50 @@ class AndroidBudget(unittest.TestCase):
         
         if saved == True:
             self.driver.implicitly_wait(5) # waits 5 seconds
-            self.assertEqual("teste", self.driver.find_element(By.XPATH, "//android.widget.TextView[contains(@text, 'teste')]").get_attribute('text'))
+            self.assertEqual(test_names[3], self.driver.find_element(By.XPATH, "//android.widget.TextView[contains(@text, 'Lucas')]").get_attribute('text'))
 
+    def test_empty_name(self):
+        test_names= [""," ",12,"Lucas"]
+        test_values= [""," ","teste_letras",1000]
+
+        if self.driver.find_element(By.XPATH, "//android.widget.TextView[contains(@text, 'Welcome to Budget Watch')]"):
+            skip = self.driver.find_element(By.ID, 'protect.budgetwatch:id/skip')
+            skip.click()
+        else: 
+            print("Skip button is not visible")
+
+        if self.driver.find_element(By.XPATH,"//android.widget.TextView[contains(@text, 'Budget Watch')]"):
+            budget = self.driver.find_element(By.XPATH,"//android.widget.TextView[contains(@text, 'Budgets')]")
+            budget.click()
+            page = True
+        else:
+            page = False 
+            print("Not able to get main page")
+
+        if page == True:
+            add = self.driver.find_element(By.ID, 'protect.budgetwatch:id/action_add')
+            add.click()
+           
+            name = self.driver.find_element(By.ID, 'protect.budgetwatch:id/budgetNameEdit')
+            name.set_text(test_names[0])
+            
+            value = self.driver.find_element(By.ID, 'protect.budgetwatch:id/valueEdit')
+            value.set_text(test_values[3])
+
+            save = self.driver.find_element(By.ID, 'protect.budgetwatch:id/action_save')
+            save.click()
+            saved=True
+            
+        else:
+            print("Budget page was not opened")
+    
+        if saved == True:
+            toast_empty = self.driver.find_element(By.ID, "protect.budgetwatch:id/snackbar_text")
+            self.assertEqual('Budget type is empty', toast_empty.get_attribute('text'))
+            self.driver.implicitly_wait(5) # waits 5 seconds
+           
+            
+            
     def test_app_transactions_expenses_add(self):
 
         if self.driver.find_element(By.XPATH, "//android.widget.TextView[contains(@text, 'Welcome to Budget Watch')]"):
@@ -99,6 +144,10 @@ class AndroidBudget(unittest.TestCase):
             save = self.driver.find_element(By.ID, 'protect.budgetwatch:id/action_save')
             save.click()
             saved=True
+
+        if saved == True:
+            self.driver.implicitly_wait(5) # waits 5 seconds
+            self.assertEqual("teste_expense", self.driver.find_element(By.XPATH, "//android.widget.TextView[contains(@text, 'test_expense')]").get_attribute('text'))
 
     def tearDown(self):
         self.driver.quit()
