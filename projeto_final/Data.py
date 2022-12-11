5c7b22bed6df00989cef58a7f3232cfe9c8fbc75
lucaslsg-prog <lsg.comunicacoes@gmail.com>
2022-12-11 00:47:37 -0300

    Added more test cases

--- projeto_final/Data.py
+++ projeto_final/Data.py
@@ -1,9 +1,10 @@
 class TestData(object):
     msg_input_required_empty = "Field can't be empty."
+    msg_input_over_limit = "You can't be so rich."
     valid_price = "250"
     max_valid_characters_price = "1234567891011" #should allow 13 characters
     over_limit_characters_price = "12345678910111" #should not allow 14 characters
     invalid_price_with_letter = "123abc"
-
+    invalid_category_with_number = "123"
     valid_title = "Test_Title"
     valid_category = "Test_Category"
--- projeto_final/TestCases.py
+++ projeto_final/TestCases.py
@@ -15,7 +15,7 @@ class MyTestCase(unittest.TestCase):
         desired_caps = {}
         desired_caps['platformName'] = 'Android'
         desired_caps['deviceName'] = 'emulator-5554'
-        desired_caps['appPackage'] = 'Money Track'
+        desired_caps['appPackage'] = 'moneytracker'
         desired_caps['appActivity'] = '.MainActivity'
         self.driver = webdriver.Remote('http://127.0.0.1:4723/wd/hub', desired_caps)
 
@@ -31,6 +31,82 @@ class MyTestCase(unittest.TestCase):
         income_page.save_new_register()
         self.driver.implicitly_wait(30)
         self.assertEqual(records_page.get_title(), TestData.valid_title)
+
+    def test_add_new_price_max_characters(self):
+        records_page = ViewRecordsPage(self.driver)
+        records_page.open_add_income_page()
+        self.driver.implicitly_wait(30)
+        income_page = AddRegisterPages(self.driver)
+        income_page.insert_price(TestData.max_valid_characters_price)
+        income_page.insert_title(TestData.valid_title)
+        income_page.insert_category(TestData.valid_category)
+        self.driver.implicitly_wait(30)
+        income_page.save_new_register()
+        self.driver.implicitly_wait(30)
+        self.assertEqual(records_page.get_title(), TestData.valid_title)
+        
+    def test_add_new_price_over_limit_characters(self):
+        records_page = ViewRecordsPage(self.driver)
+        records_page.open_add_income_page()
+        self.driver.implicitly_wait(30)
+        income_page = AddRegisterPages(self.driver)
+        income_page.insert_price(TestData.over_limit_characters_price)
+        income_page.insert_title(TestData.valid_title)
+        income_page.insert_category(TestData.valid_category)
+        self.driver.implicitly_wait(30)
+        income_page.save_new_register()
+        self.driver.implicitly_wait(30)
+        self.assertEqual(income_page.get_errorr(), TestData.msg_input_over_limit)
+        
+    def test_add_new_price_with_letter(self):
+        records_page = ViewRecordsPage(self.driver)
+        records_page.open_add_income_page()
+        self.driver.implicitly_wait(30)
+        income_page = AddRegisterPages(self.driver)
+        income_page.insert_price(TestData.invalid_price_with_letter)
+        income_page.insert_title(TestData.valid_title)
+        income_page.insert_category(TestData.valid_category)
+        self.driver.implicitly_wait(30)
+        income_page.save_new_register()
+        self.driver.implicitly_wait(30)
+        self.assertEqual(income_page.get_errorr(), TestData.msg_input_required_empty)
+        
+    def test_add_new_price_empty(self):
+        records_page = ViewRecordsPage(self.driver)
+        records_page.open_add_income_page()
+        self.driver.implicitly_wait(30)
+        income_page = AddRegisterPages(self.driver)
+        income_page.insert_title(TestData.valid_title)
+        income_page.insert_category(TestData.valid_category)
+        self.driver.implicitly_wait(30)
+        income_page.save_new_register()
+        self.driver.implicitly_wait(30)
+        self.assertEqual(income_page.get_errorr(), TestData.msg_input_required_empty)
+        
+    def test_add_new_category_with_numbers(self):
+        records_page = ViewRecordsPage(self.driver)
+        records_page.open_add_income_page()
+        self.driver.implicitly_wait(30)
+        income_page = AddRegisterPages(self.driver)
+        income_page.insert_price(TestData.valid_price)
+        income_page.insert_title(TestData.valid_title)
+        income_page.insert_category(TestData.invalid_category_with_number)
+        self.driver.implicitly_wait(30)
+        income_page.save_new_register()
+        self.driver.implicitly_wait(30)
+        self.assertFalse(records_page.get_title(), TestData.valid_title)
+        
+    def test_add_new_category_empty(self):
+        records_page = ViewRecordsPage(self.driver)
+        records_page.open_add_income_page()
+        self.driver.implicitly_wait(30)
+        income_page = AddRegisterPages(self.driver)
+        income_page.insert_price(TestData.valid_price)
+        income_page.insert_title(TestData.valid_title)
+        self.driver.implicitly_wait(30)
+        income_page.save_new_register()
+        self.driver.implicitly_wait(30)
+        self.assertEqual(income_page.get_errorr(), TestData.msg_input_required_empty)
         
 if __name__ == '__main__':
     unittest.main()
